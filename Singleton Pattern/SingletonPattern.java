ce38e6685a50fab1fe53452d3910bf2a0751925c
Gurunadh Paidipoolu <gurunadh.p@HCL.COM>
2017-04-12 13:01:13 +0530

    Singleton Pattern

--- Singleton Pattern/SingletonPattern.java
+++ Singleton Pattern/SingletonPattern.java
@@ -1,5 +1,6 @@
-
+import java.io.Serializable;
 public class SingletonPattern {
+private static final long serialVersionUID = -7604766932017737115L;
 private static SingletonPattern instance;
 
 private SingletonPattern(){
@@ -15,5 +16,8 @@ public static SingletonPattern getInstance(){
 	if(instance==null)
 		instance=SingletonHelper.INSTANCE;
 	return instance;
+}
+protected Object readResolve() {
+    return getInstance();
 }
 }
